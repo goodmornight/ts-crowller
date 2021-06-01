7383be7d4cf7d65a48a18aeb27fc5b2d98c53354
GoodNight <goodnightlilanhui@gmail.com>
2021-06-01 13:19:02 +0800

    feat: get useful data

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,14 +1,39 @@
 import superagent from 'superagent';
+import cheerio from 'cheerio';
+
+interface Course {
+  title: string,
+  count: number
+}
 
 class Crowller {
   private secret = 'x3b174jsx';
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
-  private rawHtml = '';
+
+  getCourseInfo(html: string) {
+    const $ = cheerio.load(html);
+    const courseItems = $('.course-item');
+    const courseInfos: Course[] = [];
+    courseItems.map((indx, elem) => {
+      const desc = $(elem).find('.course-desc');
+      const title = desc.eq(0).text();
+      const count = parseInt(desc.eq(1).text().split('：')[1], 10);
+      courseInfos.push({
+        title,
+        count
+      })
+    });
+    
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos
+    }
+    console.log(result)
+  }
 
   async getRawHtml() {
     const result = await superagent.get(this.url);
-    console.log(result.text);
-    this.rawHtml = result.text;
+    this.getCourseInfo(result.text);
   }
 
   constructor () {
