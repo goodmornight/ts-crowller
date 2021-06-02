c7820b0cd521044bbf45758ebc0bbedce95f031e
GoodNight <goodnightlilanhui@gmail.com>
2021-06-02 09:40:50 +0800

    perf: filePath

--- src/crowller.ts
+++ src/crowller.ts
@@ -20,6 +20,7 @@ interface Content {
 class Crowller {
   private secret = 'x3b174jsx';
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
+  private filePath = path.resolve(__dirname, '../data/course.json');
 
   getCourseInfo(html: string) {
     const $ = cheerio.load(html);
@@ -42,21 +43,19 @@ class Crowller {
   }
 
   generateJsonContent(courseInfo: CourseInfo) {
-    const filePath = path.resolve(__dirname, '../data/course.json');
     let fileContent: Content = {};
-    if(fs.existsSync(filePath)) {
-      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+    if(fs.existsSync(this.filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
     }
     fileContent[courseInfo.time] = courseInfo.data;
     return fileContent;
   }
 
   async initSpiderProcess() {
-    const filePath = path.resolve(__dirname, '../data/course.json');
     const html = await this.getRawHtml();
     const courseInfo = this.getCourseInfo(html);
     const fileContent = this.generateJsonContent(courseInfo);
-    fs.writeFileSync(filePath, JSON.stringify(fileContent));
+    fs.writeFileSync(this.filePath, JSON.stringify(fileContent));
   }
 
   async getRawHtml() {
