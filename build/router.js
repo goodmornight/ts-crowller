5b58f360bc4a68bb69926191a7c1634f77fd4c36
GoodNight <goodnightlilanhui@gmail.com>
2021-06-03 10:40:41 +0800

    feat: add body-parser

--- build/router.js
+++ build/router.js
@@ -8,13 +8,15 @@ var crowller_1 = __importDefault(require("./crowller"));
 var dellAnalyzer_1 = __importDefault(require("./dellAnalyzer"));
 var router = express_1.Router();
 router.get('/', function (req, res) {
-    res.send('hello world');
+    res.send("\n  <html>\n    <body>\n      <form method=\"post\" action=\"/getData\">\n        <input type=\"password\" name=\"password\" />\n        <button>\u63D0\u4EA4</button>\n      </form>\n    </body>\n  </html>\n  ");
 });
-router.get('/getData', function (req, res) {
-    var secret = 'x3b174jsx';
-    var url = "http://www.dell-lee.com/typescript/demo.html?secret=" + secret;
-    var analyzer = dellAnalyzer_1.default.getInstance();
-    new crowller_1.default(url, analyzer);
-    res.send('获取数据');
+router.post('/getData', function (req, res) {
+    if (req.body.password === '123') {
+        var secret = 'x3b174jsx';
+        var url = "http://www.dell-lee.com/typescript/demo.html?secret=" + secret;
+        var analyzer = dellAnalyzer_1.default.getInstance();
+        new crowller_1.default(url, analyzer);
+        res.send('获取数据');
+    }
 });
 exports.default = router;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import bodyParser from 'body-parser';
 import router from './router'
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router)
 
 app.listen(7001, () => {
